9424efd8c20dc1a373d4d60a3809742f249538e5
Heba Sajid <hsajid35467@gmail.com>
2022-10-24 16:45:47 -0400

    buttons fixed

---
+++ server/config/config.js
@@ -0,0 +1,3 @@
+export const Secret = "someSecret";
+//connecting to MongoDB
+export const MongoURI = "mongodb+srv://hsajid35467:183g9k2m1w@comp229-midterm1.aquhjfw.mongodb.net/?retryWrites=true&w=majority";
